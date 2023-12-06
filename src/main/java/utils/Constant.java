a0ac44607b59f767daa96698e4bd2b3d413667fe
minhtrong1806 <dominhtrong1806@gmail.com>
2023-12-06 14:42:08 +0700

    them utils/Constant.java va utils/UploadUtil.java

---
+++ src/main/java/utils/Constant.java
@@ -0,0 +1,8 @@
+package utils;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class Constant {
+	public static final String DIR = "D:\\HOC\\2023_2024\\HK1\\lap_trinh_web\\DoAn-Workspace\\DuckStore\\src\\main\\webapp\\views\\images";
+}
